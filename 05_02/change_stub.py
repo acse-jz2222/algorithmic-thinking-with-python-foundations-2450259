7220ae308a18261cccd668414c5ae74bfc0a38d7
Jiayi Zhang <jiayi.zhang22@imperial.ac.uk>
2022-11-05 19:44:36 +0000

    Implement change

--- 05_02/change_stub.py
+++ 05_02/change_stub.py
@@ -1,5 +1,24 @@
+"""
+minimum set of coins
+"""
+
+
+import numpy as np
+
+
 def make_change(target_amount):
-    pass  # Write your solution here. 
+    coin_type = [200, 100, 50, 20, 10, 5, 2, 1]
+
+    change = np.zeros_like(coin_type)
+
+    for i in range(len(coin_type)):
+        coin_count = 0
+        if target_amount >= coin_type[i]:
+            coin_count += int(target_amount / coin_type[i])
+            change[i] = coin_count
+            target_amount -= coin_count * coin_type[i]
+
+    return coin_type * change
 
 
 print(make_change(24))  # 3: 20p + 2p + 2p
