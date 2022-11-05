c8884baf0a6a526cabb6681a02ad6cbabbcf0542
Jiayi Zhang <jiayi.zhang22@imperial.ac.uk>
2022-11-05 20:35:07 +0000

    Implement dijkistra with priority queue with online tutorial

--- 05_07/dijkstra_1.py
+++ 05_07/dijkstra_1.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 INF = float("infinity")
 
 # Define graph as dictionary representing adjacency list.
@@ -46,5 +49,28 @@ while len(unvisited_min_distances) > 0:
     # Remove the current vertex from the unvisited list (dictionary).
     del unvisited_min_distances[current_vertex]
 
+
+def shortest_path_dijkistra(graph, u):
+    distances = {vertex: INF for vertex in graph}
+    distances[u] = 0
+    pq = [(0, u)]
+
+    while len(pq) > 0:
+        current_dist, current_vertex = heapq.heappop(pq)
+
+        if current_dist > distances[current_vertex]:
+            continue
+
+        for neighbour, neighbour_dist in graph[current_vertex].items():
+            distance = current_dist + neighbour_dist
+
+            if distance < distances[neighbour]:
+                distances[neighbour] = distance
+                heapq.heappush(pq, (distance, neighbour))
+
+    return distances
+
+
 # Display results.
 print(sorted(visited_vertices.items()))
+print(shortest_path_dijkistra(graph, "U"))
