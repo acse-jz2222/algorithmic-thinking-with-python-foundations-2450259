73eb4f04063fa1f9d7de549910310a269039d96f
Jiayi Zhang <jiayi.zhang22@imperial.ac.uk>
2022-11-05 14:55:27 +0000

    Add comments

--- 01_01/100_doors_solution.py
+++ 01_01/100_doors_solution.py
@@ -1,7 +1,7 @@
 doors = [False] * 101  # So we can start at door no. 1 We will ignore index 0.
 
 for i in range(1, 101):
-    for j in range(i, 101, i):
+    for j in range(i, 101, i):  # inner loop start with i!
         doors[j] = not doors[j]
 
 for i in range(1, 101):
